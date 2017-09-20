8c8c96caaecab2ba60e1ce08a6a9a12837875f18
Mary Kate Fain <mkfain1@gmail.com>
2017-09-20 10:06:12 -0400

    dim background when form is up

--- www/assets/main.js
+++ www/assets/main.js
@@ -1,7 +1,9 @@
 function showDiv() {
   document.getElementById('hidden_form').style.display = "block";
+  document.getElementById("dim-box").style.display = "block";
 }
 
 function hideDiv() {
   document.getElementById('hidden_form').style.display = "none";
+  document.getElementById("dim-box").style.display = "none";
 }
